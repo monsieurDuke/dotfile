0336ada1e4906ffea356336770ed229a114505d4
monsieurDuke <icatmuhammad2@gmail.com>
2023-07-30 12:35:11 +0700

    recovering file #2

--- .config/qtile/scripts/widget.sh
+++ .config/qtile/scripts/widget.sh
@@ -15,16 +15,14 @@ mplayer_cam() { ## -c
 }
 #==========================
 ffmpeg_record() { ## -f \\ start & stop
-	wid=$(cat /sys/class/graphics/fb0/virtual_size | cut -d ',' -f 1)
-	hei=$(cat /sys/class/graphics/fb0/virtual_size | cut -d ',' -f 2)
-	res=$(echo $wid\x$hei)
+	res=$(xrandr | grep '*' | awk '{ print $1 }')
 	case $1 in
 		"start")
-			ffmpeg -video_size $res -framerate 60 -f x11grab -i :0.0+0,0 -f alsa -ac 2 -i pulse -acodec aac -strict experimental -c:v libx264rgb -crf 0 -preset ultrafast /home/icat/Videos/$(date +"%H_%M_%S--%d_%m_%y").flv & nohup
-			spd-say "recording started"
+			ffmpeg -video_size $res -framerate 60 -f x11grab -i :0.0+0,0 -f alsa -ac 2 -i pulse -acodec aac -strict experimental -c:v libx264rgb -crf 0 -preset ultrafast /home/icat/media/$(date +"%H_%M_%S--%d_%m_%y").flv &
+			espeak "recording started"
 			;;
 		"stop")
-			spd-say "recording stopped" & wait
+			espeak "recording stopped" & wait
 			sleep 2s
 			killall ffmpeg	
 			;;
